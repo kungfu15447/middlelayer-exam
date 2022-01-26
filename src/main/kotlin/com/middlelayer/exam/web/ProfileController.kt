7da70062fcfc930dad68201d6581dbd2a8442e8f
Jørgen H <34099284+Schweizeren@users.noreply.github.com>
2022-01-26 12:09:07 +0100

    Now gets the profile and returns it dynamically

--- src/main/kotlin/com/middlelayer/exam/web/ProfileController.kt
+++ src/main/kotlin/com/middlelayer/exam/web/ProfileController.kt
@@ -1,60 +1,40 @@
 package com.middlelayer.exam.web
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.middlelayer.exam.core.models.Profile
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.internal.addHeaderLenient
-import okhttp3.internal.http2.Header
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.context.request.async.DeferredResult
 import java.net.URL
+import com.fasterxml.jackson.databind.node.*
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.middlelayer.exam.core.models.Profile
 
 @RestController
 class ProfileController {
 
-    fun getProfileXsi() {
+    fun getProfileXsi(authorization: String, userid: String) : Profile {
         val client = OkHttpClient()
-        val url = URL("https://scalexi-pp-xsp2.tdc.dk/com.broadsoft.xsi-actions/v2.0/user/PA_4523706117/profile/")
+        val url = URL("https://scalexi-pp-xsp2.tdc.dk/com.broadsoft.xsi-actions/v2.0/user/$userid/profile/")
 
         val request = Request.Builder()
             .url(url)
             .get()
-            .header("Authorization","Basic bTIzNzA2MTE3QHZrMTAzNzQxLmh2b2lwLmRrOjExMTExMQ==")
+            .header("Authorization", authorization)
             .build()
         val response = client.newCall(request).execute()
 
+        // Xml String
         val responseBody = response.body!!.string()
 
+        val mapper: XmlMapper = XmlMapper();
 
-
-        println("Response Body: " + responseBody)
-
-
-
-        val mapperAll = ObjectMapper()
-        val objData = mapperAll.readTree(responseBody)
-        println(objData)
-
+        val profile: Profile = mapper.readValue(responseBody)
+        return profile;
 
     }
 
-    @GetMapping("tryMethod")
-    fun getProfile() {
-        getProfileXsi();
+    @GetMapping("{userid}/profile")
+    fun getProfile(@RequestHeader("Authorization") authorization: String, @PathVariable userid: String) : Profile {
+        var profile: Profile = getProfileXsi(authorization, userid);
+        return profile;
     }
-
-    /*
-
-    data class AdditionalDetails(val emailAddress: String?, val mobile: String?, val location: String?, val department: Department?, val impId: String?, val yahooId: String?)
-
-data class Base(val Profile: Profile?)
-
-data class Department(val xsi:nil: String?)
-
-data class Details(val firstName: String?, val lastName: String?, val hiranganaFirstName: String?, val number: String?, val extension: String?, val hiranganaLastName: String?, val groupId: String?, val serviceProvider: ServiceProvider?, val userId: String?)
-
-data class Profile(val xmlns: String?, val registrations: String?, val scheduleList: String?, val timeZoneDisplayName: String?, val portalPasswordChange: String?, val countryCode: String?, val fac: String?, val timeZone: String?, val xmlns:xsi: String?, val details: Details?, val additionalDetails: AdditionalDetails?, val passwordExpiresDays: String?)
-
-data class ServiceProvider(val isEnterprise: String?, val content: String?)
-     */
 }
