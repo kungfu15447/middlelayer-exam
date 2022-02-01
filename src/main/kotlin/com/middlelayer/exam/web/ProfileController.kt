3684d79d657c0ccfe9acfe47526cd88c00ed982a
Frederik Jensen <44639732+kungfu15447@users.noreply.github.com>
2022-02-01 15:36:51 +0100

    now makes token and checks headers for that token. Needs to make code more clean

--- src/main/kotlin/com/middlelayer/exam/web/ProfileController.kt
+++ src/main/kotlin/com/middlelayer/exam/web/ProfileController.kt
@@ -1,9 +1,12 @@
 package com.middlelayer.exam.web
 import com.middlelayer.exam.core.interfaces.infrastructure.IProfileRepository
+import com.middlelayer.exam.core.models.Profile
 import com.middlelayer.exam.infrastructure.ProfileRepository
 import org.springframework.web.bind.annotation.*
 import com.middlelayer.exam.service.AuthService
 import io.jsonwebtoken.Jwts
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 
 @RestController
@@ -13,17 +16,14 @@ class ProfileController {
     private val profileRepository: IProfileRepository = ProfileRepository()
 
     @GetMapping("user/{userid}/profile")
-    fun getProfile(@RequestHeader("jwt") jwt: String?,@RequestHeader("Authorization") authorization: String, @PathVariable userid: String) : ResponseEntity<Any> {
+    fun getProfile(@RequestHeader("Authorization") authorization: String, @PathVariable userid: String) : ResponseEntity<Profile> {
 
-        if(jwt.isNullOrEmpty()) {
-            val jwtToken = auth.register(authorization,userid)
-            return ResponseEntity.status(401).body(jwtToken);
-        }
+        val profile = profileRepository.getProfileXsi(authorization, userid)
+        val headers = HttpHeaders()
+        headers.add("Authorization", "Bearer ${auth.register(authorization, userid)}")
+        var response = ResponseEntity<Profile>(profile, headers,HttpStatus.OK)
 
-        val jwtBody = Jwts.parser().setSigningKey("cfd0209b-7a93-482f-97d0-cd9d368a5533").parseClaimsJws(jwt).body
-        val jwtAuthorization: String = jwtBody["authorization"].toString()
-
-        return ResponseEntity.ok(profileRepository.getProfileXsi(jwtAuthorization,userid))
+        return response
     }
 
     @GetMapping("user/test")
--- src/main/kotlin/com/middlelayer/exam/web/filters/AuthFilter.kt
+++ src/main/kotlin/com/middlelayer/exam/web/filters/AuthFilter.kt
@@ -1,15 +1,17 @@
 package com.middlelayer.exam.web.filters
 
+import io.jsonwebtoken.JwtException
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 import org.springframework.security.web.util.matcher.RequestMatcher
 import org.springframework.web.filter.GenericFilterBean
+import java.util.*
 import javax.servlet.FilterChain
 import javax.servlet.ServletRequest
 import javax.servlet.ServletResponse
 import javax.servlet.http.HttpServletRequest
-
-
-
+import javax.servlet.http.HttpServletResponse
 
 
 class AuthFilter : GenericFilterBean {
@@ -21,13 +23,29 @@ class AuthFilter : GenericFilterBean {
     }
 
     override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
-        val path = (request as HttpServletRequest).servletPath
-        if (filterUrl?.matches(request) == true) {
-            println("Creating token")
-        } else {
-            println("Checking token")
+        var req = request as HttpServletRequest
+        var res = response as HttpServletResponse
+        var isAuthorized = true
+        if (filterUrl?.matches(req) != true) {
+            try {
+                var token = req.getHeader("Authorization")
+                if (token != null) {
+                    token = token.replace("Bearer", "")
+                    var jws = Jwts.parser()
+                        .setSigningKey("cfd0209b-7a93-482f-97d0-cd9d368a5533")
+                        .parseClaimsJws(token)
+                    println(jws)
+                } else {
+                    isAuthorized = false
+                }
+            } catch (ex: JwtException) {
+                isAuthorized = false
+            }
         }
-        println(path)
-        chain?.doFilter(request, response)
+        if (!isAuthorized) {
+            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized access")
+        }else {
+            chain?.doFilter(req, res)
+        }
     }
 }
