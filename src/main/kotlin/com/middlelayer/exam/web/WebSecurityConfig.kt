921fdf6c359ddbe9680c2260d52aced7236c8831
Frederik Jensen <44639732+kungfu15447@users.noreply.github.com>
2022-02-02 12:24:38 +0100

    cors is now implemented correctly

--- src/main/kotlin/com/middlelayer/exam/web/WebSecurityConfig.kt
+++ src/main/kotlin/com/middlelayer/exam/web/WebSecurityConfig.kt
@@ -6,14 +6,29 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfig : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity?) {
-        http?.cors {
-            it.and()
-                ?.addFilterAfter(AuthFilter(), BasicAuthenticationFilter::class.java)
-        }
+        http
+            ?.csrf()?.disable()
+            ?.cors()?.configurationSource(setCors())
+            ?.and()
+            ?.addFilterAfter(AuthFilter(), BasicAuthenticationFilter::class.java)
+    }
+
+    private fun setCors(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("http://localhost:6000")
+        configuration.allowedMethods = listOf("*")
+        configuration.allowedHeaders = listOf("*")
+        configuration.allowCredentials = false
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
