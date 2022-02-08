c7c4ca328d827aeb3a4d332075147dd1587878f3
Frederik Jensen <44639732+kungfu15447@users.noreply.github.com>
2022-02-08 11:50:18 +0100

    Quick error handling implementation

--- src/main/kotlin/com/middlelayer/exam/infrastructure/XsiClient.kt
+++ src/main/kotlin/com/middlelayer/exam/infrastructure/XsiClient.kt
@@ -1,14 +1,14 @@
 package com.middlelayer.exam.infrastructure
 
+import com.middlelayer.exam.core.exceptions.BadRequestException
+import com.middlelayer.exam.core.exceptions.NotFoundException
+import com.middlelayer.exam.core.exceptions.UnauthorizedException
 import com.middlelayer.exam.core.interfaces.infrastructure.IXsiClient
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.*
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
-import org.springframework.web.client.RestTemplate
-import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.awaitExchange
-import org.springframework.web.reactive.function.client.bodyToMono
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.reactive.function.client.*
 import reactor.core.publisher.Mono
 
 @Component
@@ -21,7 +21,18 @@ class XsiClient : IXsiClient {
         webClient = WebClient
                         .builder()
                         .baseUrl(server)
+                        .filter(ExchangeFilterFunction.ofResponseProcessor(this::handleApiErrorResponse))
                         .build()
+    }
+
+    private fun handleApiErrorResponse(clientResponse: ClientResponse): Mono<ClientResponse> {
+        println("Something something")
+        var statusCode = clientResponse.statusCode()
+        when(statusCode) {
+            HttpStatus.NOT_FOUND -> throw NotFoundException("Route could not be found")
+            HttpStatus.UNAUTHORIZED -> throw UnauthorizedException("Tried to access unauthorized route")
+        }
+        return Mono.just(clientResponse)
     }
 
     override fun get(uri: String, auth: String?): String? {
