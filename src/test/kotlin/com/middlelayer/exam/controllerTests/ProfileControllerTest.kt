8f3a0bea738d6a78a023dd2ede335fcc29cc347d
Frederik Jensen <44639732+kungfu15447@users.noreply.github.com>
2022-03-02 14:37:24 +0100

    a lot of tests for failing login

--- src/test/kotlin/com/middlelayer/exam/controllerTests/ProfileControllerTest.kt
+++ src/test/kotlin/com/middlelayer/exam/controllerTests/ProfileControllerTest.kt
@@ -2,6 +2,8 @@ package com.middlelayer.exam.controllerTests
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.middlelayer.exam.core.interfaces.service.IAuthService
 import com.middlelayer.exam.core.interfaces.service.IProfileService
+import com.middlelayer.exam.core.models.domain.DProfile
+import com.middlelayer.exam.core.models.domain.DService
 import com.middlelayer.exam.web.ProfileController
 import com.middlelayer.exam.web.dto.profile.LoginDTO
 import org.junit.jupiter.api.BeforeEach
@@ -9,6 +11,8 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.context.junit.jupiter.SpringExtension
@@ -39,7 +43,6 @@ class ProfileControllerTest {
     @CsvSource(
         ",",
         "'',''",
-        "' ',' '",
         "username,",
         ",password",
     )
@@ -56,5 +59,127 @@ class ProfileControllerTest {
 
         //Assert
         response.expectStatus().isBadRequest
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        ",",
+        "'',''",
+        "username,",
+        ",password",
+    )
+    fun `on Login with invalid body returns correct error message`(username: String?, password: String?) {
+        //Assign
+        val requestBody = LoginDTO(username, password)
+
+        //Act
+        var response = web.post()
+            .uri("/api/user/profile/login")
+            .body(BodyInserters.fromValue(requestBody))
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .returnResult(String::class.java)
+            .responseBody.blockFirst()
+
+        //Assert
+        assert(response == "Username and/or password cannot be null or empty")
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        ",",
+        "'',''",
+        "username,",
+        ",password",
+    )
+    fun `on Login with invalid body never calls getProfile`(username: String?, password: String?) {
+        //Assign
+        val requestBody = LoginDTO(username, password)
+
+        //Act
+        var response = web.post()
+            .uri("/api/user/profile/login")
+            .body(BodyInserters.fromValue(requestBody))
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .returnResult(String::class.java)
+            .responseBody.blockFirst()
+
+        //Assert
+        verify(profileService, times(0)).getProfile("", "")
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        ",",
+        "'',''",
+        "username,",
+        ",password",
+    )
+    fun `on Login with invalid body never calls getServicesFromProfile`(username: String?, password: String?) {
+        //Assign
+        val requestBody = LoginDTO(username, password)
+
+        //Act
+        var response = web.post()
+            .uri("/api/user/profile/login")
+            .body(BodyInserters.fromValue(requestBody))
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .returnResult(String::class.java)
+            .responseBody.blockFirst()
+
+        //Assert
+        verify(profileService, times(0)).getServicesFromProfile("", "")
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        ",",
+        "'',''",
+        "username,",
+        ",password",
+    )
+    fun `on Login with invalid body never calls createBasicAuthToken`(username: String?, password: String?) {
+        //Assign
+        val requestBody = LoginDTO(username, password)
+
+        //Act
+        var response = web.post()
+            .uri("/api/user/profile/login")
+            .body(BodyInserters.fromValue(requestBody))
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .returnResult(String::class.java)
+            .responseBody.blockFirst()
+
+        //Assert
+        verify(authService, times(0)).createBasicAuthToken("", "")
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        ",",
+        "'',''",
+        "username,",
+        ",password",
+    )
+    fun `on Login with invalid body never calls register`(username: String?, password: String?) {
+        //Assign
+        val requestBody = LoginDTO(username, password)
+
+        //Act
+        var response = web.post()
+            .uri("/api/user/profile/login")
+            .body(BodyInserters.fromValue(requestBody))
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .returnResult(String::class.java)
+            .responseBody.blockFirst()
+
+        //Assert
+        val profile = DProfile("", "", "", "", "", "", "")
+        var services = ArrayList<DService>()
+        verify(authService, times(0)).register("", profile, services)
     }
 }
